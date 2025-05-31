5a002cdb4d595f8e8e3805cb2d90b836567a498e
vikramChaurasiya <bhartivikram633@gmail.com>
2025-05-31 13:24:46 +0530

    css fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-        <footer className="footer sm:footer-horizontal bg-base-200 text-neutral-content  p-4  fixed bottom-0">
+        <footer className="footer sm:footer-horizontal bg-base-200 text-neutral-content  p-4  absolute bottom-0">
             <aside className="grid-flow-col items-center">
             <svg
                 width="36"
